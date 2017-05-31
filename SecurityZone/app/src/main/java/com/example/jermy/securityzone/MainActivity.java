c8964abd26b19f2e50eb742c8666d8b96257cadd
DaleBae <jermy0131@gmail.com>
2017-06-01 00:54:42 +0900

    06.01

--- SecurityZone/app/src/main/java/com/example/jermy/securityzone/MainActivity.java
+++ SecurityZone/app/src/main/java/com/example/jermy/securityzone/MainActivity.java
@@ -1,13 +1,20 @@
 package com.example.jermy.securityzone;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+    ImageView bicycle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
+    }
+    public void init(){
+        bicycle=(ImageView)findViewById(R.id.bicycle);
+        bicycle.setImageResource(R.drawable.img1);
     }
 }
