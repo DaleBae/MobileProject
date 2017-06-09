6c371e2a9e6c35c493da537345544f8fff5c9452
DaleBae <jermy0131@gmail.com>
2017-06-09 22:58:09 +0900

    json in

--- SecurityZone/app/src/main/java/com/example/jermy/securityzone/MenuActivity.java
+++ SecurityZone/app/src/main/java/com/example/jermy/securityzone/MenuActivity.java
@@ -7,34 +7,22 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.util.Scanner;
-
 public class MenuActivity extends AppCompatActivity {
     Button btn1;
     Button btn2;
-    DatabaseReference rDatabase;
-    DatabaseReference customerTable;
-    FirebaseDatabase database;
+/*    FirebaseDatabase database;
     int cctvcount;
-    private FirebaseAuth mAuth;
+    private FirebaseAuth mAuth;*/
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
         init();
-        initDB();
+//        initDB();
     }
     public void init(){
-        cctvcount=0;
+//        cctvcount=0;
         btn1=(Button)findViewById(R.id.btn1);
         btn2=(Button)findViewById(R.id.btn2);
         btn1.setOnTouchListener(new View.OnTouchListener() {
@@ -69,51 +57,61 @@ public class MenuActivity extends AppCompatActivity {
                 return false;
             }
         });
-        mAuth= FirebaseAuth.getInstance();
-        mAuth.signInWithEmailAndPassword("jermy0131@naver.com","wpfmal1234");
+/*        mAuth= FirebaseAuth.getInstance();
+        mAuth.signInWithEmailAndPassword("jermy0131@naver.com","wpfmal1234");*/
     }
-    public void initDB(){
+/*    public void initDB(){
         database = FirebaseDatabase.getInstance();
         String result = "";
-        Scanner scanner = new Scanner(getResources().openRawResource(R.raw.cctv));
+        Scanner scanner = new Scanner(getResources().openRawResource(R.raw.store));
         while (scanner.hasNextLine()) {
             String str = scanner.nextLine();
             result += str;
         }
         parsingJson(result);
-    }
-    public void parsingJson(String result) {
+    }*/
+/*    public void parsingJson(String result) {
         try {
-            DatabaseReference table = database.getReference("SecurityZone/cctv");
+            DatabaseReference table = database.getReference("SecurityZone/store");
 
             JSONObject json = new JSONObject(result);
-            JSONArray array = json.getJSONArray("cctv");
+            JSONArray array = json.getJSONArray("store");
             for (int i = 0; i < array.length(); i++) {
                 DatabaseReference cctv=table.child("num"+cctvcount);
-                String col1 = array.getJSONObject(i).getString("managename");
+                String col1 = array.getJSONObject(i).getString("storeName");
                 String col2 = array.getJSONObject(i).getString("Address1");
-                String col3 = array.getJSONObject(i).getString("purpose");
-                String col4 = array.getJSONObject(i).getString("numberofcam");
-                String col5 = array.getJSONObject(i).getString("dataduration");
-                String col6 = array.getJSONObject(i).getString("setDate");
-                String col7 = array.getJSONObject(i).getString("managecontact");
-                String col8 = array.getJSONObject(i).getString("latitude");
-                String col9 = array.getJSONObject(i).getString("longitude");
-                cctv.child("managename").setValue(col1);
+                String col3 = array.getJSONObject(i).getString("Address2");
+                String col4 = array.getJSONObject(i).getString("latitude");
+                String col5 = array.getJSONObject(i).getString("longitude");
+                String col6 = array.getJSONObject(i).getString("capacity");
+                String col7 = array.getJSONObject(i).getString("setYear");
+                String col8 = array.getJSONObject(i).getString("setType");
+                String col9 = array.getJSONObject(i).getString("Awnings");
+                String col10 = array.getJSONObject(i).getString("Airinjector");
+                String col11 = array.getJSONObject(i).getString("AirinjectorType");
+                String col12 = array.getJSONObject(i).getString("fix");
+                String col13 = array.getJSONObject(i).getString("managecontact");
+                String col14 = array.getJSONObject(i).getString("managename");
+                cctv.child("storeName").setValue(col1);
                 cctv.child("Address1").setValue(col2);
-                cctv.child("purpose").setValue(col3);
-                cctv.child("numberofcam").setValue(col4);
-                cctv.child("dataduration").setValue(col5);
-                cctv.child("setDate").setValue(col6);
-                cctv.child("managecontact").setValue(col7);
-                cctv.child("latitude").setValue(col8);
-                cctv.child("longitude").setValue(col9);
+                cctv.child("Address2").setValue(col3);
+                cctv.child("latitude").setValue(col4);
+                cctv.child("longitude").setValue(col5);
+                cctv.child("capacity").setValue(col6);
+                cctv.child("setYear").setValue(col7);
+                cctv.child("setType").setValue(col8);
+                cctv.child("Awnings").setValue(col9);
+                cctv.child("Airinjector").setValue(col10);
+                cctv.child("AirinjectorType").setValue(col11);
+                cctv.child("fix").setValue(col12);
+                cctv.child("managecontact").setValue(col13);
+                cctv.child("managename").setValue(col14);
                 cctvcount++;
             }
         } catch (JSONException e) {
             e.printStackTrace();
         }
-    }
+    }*/
     public void btn1Click(View view) {
         Intent intent=new Intent(this,MapActivity.class);
         startActivity(intent);
